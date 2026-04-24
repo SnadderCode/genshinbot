3da274298f27af633b09ecc34f0cc3b06a312577
Sander Kjeøy <sandergao5439@gmail.com>
2026-04-24 11:38:26 +0200

    feat: add crimson witch website data

--- functions/codeFunctions.js
+++ functions/codeFunctions.js
@@ -1,11 +1,20 @@
 module.exports = {getActivePromoCodes, getOldCodes, saveCodes};
 
 const fs = require("fs");
-const { type } = require("os");
+const {type} = require("os");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 const filePath = "./data/codes.txt";
 
 async function getActivePromoCodes() {
+	const fandomCodes = await getActiveFandomCodes() || new Map();
+	const crimsonCodes = await getActiveCrimsonCodes() || new Map();
+	const codes = new Map([...fandomCodes, ...crimsonCodes])
+	return codes;
+}
+
+async function getActiveFandomCodes() {
 	const url =
 		"https://genshin-impact.fandom.com/api.php?action=query&titles=Promotional_Code&prop=revisions&rvprop=content&rvslots=main&format=json&origin=*";
 	const response = await fetch(url);
@@ -71,31 +80,74 @@ async function getOldCodes() {
 async function saveCodes(codes) {
 	const oldCodes = await getOldCodes();
 
-  let codeList;
+	let codeList;
 
-  if (typeof codes === "undefined") {
-    return;
-  }
-  else if (typeof codes === "object" && codes !== null) {
+	if (typeof codes === "undefined") {
+		return;
+	} else if (typeof codes === "object" && codes !== null) {
 		if (codes instanceof Map) {
-			codeList = [ ...codes.keys() ];
+			codeList = [...codes.keys()];
 		} else if (Array.isArray(codes)) {
 			codeList = codes;
 		} else {
 			codeList = Object.keys(codes);
 		}
-  } else {
-    codeList = [codes];
-  }
+	} else {
+		codeList = [codes];
+	}
 
 	for (const code of codeList) {
 		if ((await oldCodes).includes(code)) {
 			continue;
 		}
 
-    fs.appendFile(filePath, code + "\r\n", function (err) {
-		if (err) throw err;
-	});
+		fs.appendFile(filePath, code + "\r\n", function (err) {
+			if (err) throw err;
+		});
 	}
 	return;
+}
+
+async function getActiveCrimsonCodes() {
+	let script;
+
+	try {
+		const response = await axios.get("https://www.crimsonwitch.com/codes/Genshin_Impact");
+		const $ = cheerio.load(response.data);
+
+		$("script").each((i, el) => {
+			const content = $(el).html();
+			if (content && content.includes("initialCodes")) script = content;
+		});
+	} catch (error) {
+		console.error("Error:", error.message);
+	}
+	if (!script) {
+		console.error("Script not found");
+		return;
+	}
+	const regex = /\\"initialCodes\\":(\[[\s\S]*?\]),\\"slug\\"/;
+	const match = script.match(regex);
+	if (!match) {
+		console.error("initialCodes not found");
+		return;
+	}
+	jsonString = match[1].replace(/\\"/g, '"');
+	const codesDict = JSON.parse(jsonString);
+
+	const codes = new Map();
+
+	for (const code of codesDict) {
+		if (code.code.toLowerCase().includes("livestream")) {
+			continue;
+		}
+
+		const codeRewards = code.rewards.map((reward) => {
+			const rewardMap = new Map();
+			rewardMap.set(reward.item, reward.qty);
+			return rewardMap;
+		});
+		codes.set(code.code, codeRewards);
+	}
+	return codes;
 }
